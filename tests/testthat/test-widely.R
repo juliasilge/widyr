b61320285cae7ea72654666fba84fea71ca38667
dgrtwo <dgrtwo@princeton.edu>
2016-05-21 00:54:47 -0400

    Improved on widely tests

--- tests/testthat/test-widely.R
+++ tests/testthat/test-widely.R
@@ -9,3 +9,10 @@ test_that("widely can widen, operate, and re-tidy", {
     expect_is(ret$item2, "character")
   }
 })
+
+test_that("widely's maximum size argument works", {
+  f <- function() {
+    widely(cor, year, country, lifeExp, maximum_size = 1000)(gapminder)
+  }
+  expect_error(f(), "1704.*large")
+})
