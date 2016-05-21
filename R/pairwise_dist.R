c1a1eeb8fc955200cb2a8d175899b6c8fe71ca5a
dgrtwo <dgrtwo@princeton.edu>
2016-05-20 21:52:42 -0400

    Fixed bugs in pairwise_dist, added tests

--- R/pairwise_dist.R
+++ R/pairwise_dist.R
@@ -9,6 +9,28 @@
 #' @param ... Extra arguments passed on to \code{\link{squarely}},
 #' such as \code{diag} and \code{upper}
 #'
+#' @examples
+#' library(gapminder)
+#'
+#' # closest countries in terms of life expectancy over time
+#' closest <- gapminder %>%
+#'   pairwise_dist(year, country, lifeExp) %>%
+#'   arrange(distance)
+#'
+#' closest
+#'
+#' closest %>% filter(item1 == "United States")
+#'
+#' # to remove duplicates, use upper = FALSE
+#' gapminder %>%
+#'   pairwise_dist(year, country, lifeExp, upper = FALSE) %>%
+#'   arrange(distance)
+#'
+#' # Can also use Manhattan distance
+#' gapminder %>%
+#'   pairwise_dist(year, country, lifeExp, method = "manhattan", upper = FALSE) %>%
+#'   arrange(distance)
+#'
 #' @export
 pairwise_dist <- function(tbl, group, item, value,
                      method = "euclidean", ...) {
@@ -22,15 +44,11 @@ pairwise_dist <- function(tbl, group, item, value,
 
 #' @rdname pairwise_dist
 #' @export
-pairwise_dist_ <- function(tbl, group, item, value,
-                       method = "euclidean",
-                      ...) {
-  method <- match.arg(method)
-
+pairwise_dist_ <- function(tbl, group, item, value, method = "euclidean", ...) {
   d_func <- squarely_(function(m) as.matrix(stats::dist(t(m), method = method)),
                       group, item, value, ...)
 
   tbl %>%
-    d_func(method = method) %>%
+    d_func() %>%
     rename(distance = value)
 }
