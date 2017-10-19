3ca9088a466e4c4ef33b4122c53586a53423f8c1
David Robinson <admiral.david@gmail.com>
2017-10-18 23:00:59 -0400

    Adding draft "widely_svd" function for dimensionality reduction (with singular value decomposition)

---
+++ R/widely_svd.R
@@ -0,0 +1,39 @@
+#' Turn into a wide matrix, perform SVD, return to tidy form
+#'
+#' This is useful for dimensionality reduction. Work in progress.
+#'
+#' @name widely_svd
+#'
+#' @param tbl Table
+#' @param item Item to perform dimensionality reduction on; will end up in \code{item} column
+#' @param feature Column describing the feature that links one item to others.
+#' @param value Value
+#' @param rank Optional; the maximum dimensionality of the data
+#'
+#' @export
+widely_svd <- function(tbl, item, feature, value, rank = NULL) {
+  widely_svd_(tbl,
+              col_name(substitute(item)),
+              col_name(substitute(feature)),
+              col_name(substitute(value)),
+              rank = rank)
+}
+
+
+#' @rdname widely_svd
+#' @export
+widely_svd_ <- function(tbl, item, feature, value, rank = NULL) {
+  perform_svd <- function(m) svd(m)$u
+
+  ret <- tbl %>%
+    widely_(perform_svd)(item, feature, value) %>%
+    transmute(item = tbl[[item]][item1],
+              dimension = item2,
+              value)
+
+  if (!is.null(rank)) {
+    ret <- filter(ret, dimension <= rank)
+  }
+
+  ret
+}
